69305f69754fcc90101084d05f7533cb497ae888
Edson A. Sensato <esensato@hotmail.com>
2022-08-24 16:00:32 -0300

    24/08/2022

--- obterPacotes.js
+++ obterPacotes.js
@@ -4,10 +4,10 @@ function main(params) {
     var auth = require('ibm-watson/auth');
     var Cloudant = require('@ibm-cloud/cloudant');
     
-    const authenticator = new auth.IamAuthenticator({apikey: 'cBmlW3by0F7Bt8CffByrPhGamibE76Al7itQ0WBR_KEK'});
+    const authenticator = new auth.IamAuthenticator({apikey: ''});
 
     var cloudant = Cloudant.CloudantV1.newInstance( {authenticator: authenticator });
-    cloudant.setServiceUrl('https://636e6945-34c2-4ece-8060-eb5dc7f92f5b-bluemix.cloudantnosqldb.appdomain.cloud');
+    cloudant.setServiceUrl('');
     
 
     return new Promise((ok, erro) => {
